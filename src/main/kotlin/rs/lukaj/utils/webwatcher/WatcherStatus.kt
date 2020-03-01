5ea2442980ec2a147bb74d47b2c8b726f9f7ebd2
Luka Jovicic <luka.jovicic16@gmail.com>
2020-03-01 18:32:26 +0100

    Add last change time to WatcherStatus

--- src/main/kotlin/rs/lukaj/utils/webwatcher/WatcherStatus.kt
+++ src/main/kotlin/rs/lukaj/utils/webwatcher/WatcherStatus.kt
@@ -5,9 +5,12 @@ class WatcherStatus(watcher : Watcher) {
         private set
     var lastInvocation = 0L
         private set
+    var lastChange = 0L
+        private set
 
     init {
         name = watcher.config.name
         lastInvocation = watcher.state.lastInvocation
+        lastChange = if(watcher.state.history.isEmpty()) 0 else watcher.state.history.last().first
     }
 }
